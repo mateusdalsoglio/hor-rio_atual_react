f861065024d3273a1fe1abdba89e5294df96e6c9
Mateus Dalsoglio <mateus.dalsoglio@yahoo.com>
2022-08-24 11:13:45 -0300

    forçar commit

---
+++ meuapp/src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+
+class App extends Component{
+
+    constructor(props){
+      super(props);
+      this.state = {
+        hora:'00:00:00'
+      };
+    }
+
+    componentDidMount(){
+      setInterval(() => {
+        this.setState({hora: new Date().toLocaleTimeString()})
+      },1000)
+    }
+
+
+
+  render(){
+      return(
+        <div>
+          <h1>Horario {this.state.hora}</h1>
+        </div>
+      );
+  }
+}
+
+export default App;
