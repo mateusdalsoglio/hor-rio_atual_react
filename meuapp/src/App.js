833f64cd06a09a5b6160a6e99e9ea337e30c751c
Mateus Dalsoglio <mateus.dalsoglio@yahoo.com>
2022-08-25 11:12:24 -0300

    forçar commit

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,30 +1,54 @@
 import React, {Component} from 'react';
 
 
-class App extends Component{
+class App extends Component {
 
-    constructor(props){
+  constructor(props){
       super(props);
       this.state = {
-        hora:'00:00:00'
+          nome: '',
+          email:'',
+          senha:'',
+          error:''
       };
-    }
 
-    componentDidMount(){
-      setInterval(() => {
-        this.setState({hora: new Date().toLocaleTimeString()})
-      },1000)
-    }
+      this.cadastrar = this.cadastrar.bind(this);
 
+  }
+
+  cadastrar(event){
+      const{nome, email, senha} = this.state;
+
+      if(nome !== '' && email !== '' && senha !== ''){
+        alert(`Nome: ${nome} \nEmail: ${email} \n senha: ${senha}`)
+      }else{
+        this.setState({error: 'Ops! Parece que esta faltando algo!'});
+      }
+
+      event.preventDefault();
+  }
 
 
   render(){
       return(
-        <div>
-          <h1>Horario {this.state.hora}</h1>
-        </div>
+          <div>
+              <h1>Novo usuario</h1>
+              {this.state.error && <p>{this.state.error}</p>}
+              <form onSubmit={this.cadastrar}>
+                  <label>Nome:</label>
+                  <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})}/><br/>
+                  <label>E-mail:</label>
+                  <input type="email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}  /><br/>
+                  <label>Senha:</label>
+                  <input type="password" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/><br/>
+                  <button type="submit">Cadastro</button>
+              </form>
+          </div>
       );
   }
 }
 
 export default App;
+
+
+
